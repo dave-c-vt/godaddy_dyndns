27e70bdfc4fa4718677880fa15596740c6079f9b
Dave Carlson <davec.vt@gmail.com>
2021-10-15 21:34:23 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# ![godaddy_dyndns](http://www.gravatar.com/avatar/42ca8c58c2bfd3de83e8c8d4261ed47e?d=mm&s=48) godaddy_dyndns!
+
+## summary
+
+this repo is made to help those who are self-hosting web services and using GoDaddy for domain listing
+service.
+
+if you have GoDaddy domains and you find yourself manually changing the @ and mx listings in GoDaddy
+every time your ISP changes your IP address, AND you run Linux, this will be helpful for you!
+
+## installation
+
+### clone this repository
+
+``` bash
+git clone https://github.com/dave-c-vt/godaddy_dyndns
+cd godaddy_dyndns
+```
+
+### get your GoDaddy API key/secret
+
+go to https://developer.godaddy.com/keys and follow the instructions there.
+
+### update the settings in the dynDns.sh file
+
+``` bash
+# GODADDY SETTINGS
+#  - see https://developer.godaddy.com/keys to create your key/secret pair
+GODADDYKEY="goDaddyKey"
+GODADDYSEC="goDaddySecret"
+
+# DOMAINS TO UPDATE
+declare -a DOMAINS=("domain1.com" "domain2.com" "domain3.cloud")
+
+
+# EMAIL SETTINGS
+SENDEMAIL=sendemail@email.com   # set to "" to not send email
+EMAILPASS=emailPassword
+SMTPSERVER=email.com
+RECVEMAIL=recpipient@someemail.com
+SUBJECT="IP ADDRESS CHANGE"
+```
+
+### add to crontab
+
+``` bash
+SCRIPT=$(pwd)/dynDns.sh
+crontab -l | grep -v "$SCRIPT" > crontab.txt && echo "* * * * * $SCRIPT" >> crontab.txt &&  crontab.txt
+```
+
+## further info
+
+- GoDaddy API usage - https://developer.godaddy.com/
---
+++ dynDns.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+
+# GODADDY SETTINGS
+#  - see https://developer.godaddy.com/keys to create your key/secret pair
+GODADDYKEY="goDaddyKey"
+GODADDYSEC="goDaddySecret"
+
+# DOMAINS TO UPDATE
+declare -a DOMAINS=("domain1.com" "domain2.com" "domain3.cloud")
+
+
+# EMAIL SETTINGS
+SENDEMAIL=sendemail@email.com   # set to "" to not send email
+EMAILPASS=emailPassword
+SMTPSERVER=email.com
+RECVEMAIL=recpipient@someemail.com
+SUBJECT="IP ADDRESS CHANGE"
+
+
+# FILENAMES
+NEWIPFILE=.new_ip
+OLDIPFILE=.old_ip
+
+dig +short myip.opendns.com @resolver1.opendns.com > .new_ip
+
+NEWIP=$(cat $NEWIPFILE)
+OLDIP=$(cat $NEWIPFILE)
+DIFF=$(diff $NEWIPFILE $OLDIPFILE)
+LOGFILE=dns.log
+
+function send_email() {
+
+    if [ ${SENDEMAIL:+1} ] 
+    then
+
+        curl --url smtp://$SMTPSERVER:587 --ssl-reqd \
+             --mail-from $SENDEMAIL \
+             --mail-rcpt $RECVEMAIL \
+             --user $SENDEMAIL:$EMAILPASS \
+             -T <(echo -e "From: $SENDEMAIL\nTo: $RECVEMAIL\nSubject: $SUBJECT\n\n$NEWIP")
+    fi
+}
+
+
+function update_dns() {
+    # uses godaddy v1 api to update dns on ip address change
+    key=$GODADDYKEY
+    secret=$GODADDYSEC
+    headers="Authorization: sso-key $key:$secret"
+
+    for domain in "${DOMAINS[@]}"
+    do
+        for name in "@" "mx"
+        do
+            echo "[$(date)] [SETTING] $domain $name $NEWIP" >> $LOGFILE
+            res=$(curl -s -X PUT "https://api.godaddy.com/v1/domains/${domain}/records/A/${name}" -H "Authorization: sso-key ${key}:${secret}" -H "Content-Type: application/json" -d " [{\"data\": \"${NEWIP}\"}]")
+            echo $res
+
+        done
+    done
+}
+
+
+if [ "$NEWIP" != "" ] && [ "$DIFF" != ""  ]
+then
+
+    echo "[$(date)] [CHANGE] new IP address: $NEWIP" >> $LOGFILE
+
+    send_email
+    update_dns
+
+    echo "$NEWIP" > $OLDIPFILE
+
+fi
+
+
